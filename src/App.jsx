2e4922069f90dfde1a5bc349df9b891d69aafea5
davidterrel <126286247+davidterrel@users.noreply.github.com>
2023-08-15 17:33:52 -0700

    fixed elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes, Route, Link } from "react-router-dom";
+import Blue from './components/Blue';
+import Red from './components/Red';
+import Home from './components/Home';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,9 +11,19 @@ function App() {
   return (
     <>
       <div id="container">
-        <h1>Hello React Router!</h1>
-        <div id="navbar">{/* navigation here */}</div>
-        <div id="main-section">{/* routes here */}</div>
+        <h1></h1>
+        <div id="navbar">
+          <li><Link to="/blue">Blue</Link></li>
+          <li><Link to="/Home">Home</Link></li>
+          <li><Link to="/red">Red</Link></li>
+        </div>
+        <div id="main-section">
+          <Routes>
+            <Route path="/blue" element={<Blue />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/red" element={<Red />} />
+          </Routes>
+        </div>
       </div>
     </>
   )
